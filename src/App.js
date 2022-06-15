8385d2970e73d8918cf5464ff906a5c2e446f5fe
Salihou-Abdoulaye <salehabdallah9910@gmail.com>
2022-06-16 01:10:49 +0200

    depot de mon projet cooreactapp

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Card from "./components/Card";
+
+const App = () => {
+  const [mealsData, setMealsData] = useState([]);
+  const [inputSearch, setInputSearch] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("https://www.themealdb.com/api/json/v1/1/search.php?s=" +inputSearch)
+      .then((res) => setMealsData(res.data.meals));
+  }, [inputSearch]);
+
+  return (
+    <div className="app-container">
+      <h1>React Cooking App</h1>
+      <input
+        type="text"
+        placeholder="Taper le nom de l'aliment (en anglais)"
+        onChange={(e) => setInputSearch(e.target.value)}
+      />
+      <div className="meals-container">
+        {mealsData &&
+          mealsData
+          .slice(0, 24)
+          .map((meal) => (
+            <Card key={meal.idMeal} meal={meal} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
